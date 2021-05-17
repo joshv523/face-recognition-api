a987ec6da478bc5b8e2aba9f7558e8c5334d4c59
Joshua Villacorta <joshmvillacorta@gmail.com>
2021-05-17 19:42:55 -0400

    updated database

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-globular-90868',
     user : 'postgres',
     password : 'joshua523',
     database : 'facerecognition'
